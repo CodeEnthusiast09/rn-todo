07c23b1139a465fe142da0767076526449ffd64e
O'Brien <obrienadedapo15@gmail.com>
2026-01-28 16:54:18 +0100

    chore: setup project
    - make app tabs style
    - understand basic things

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Setting = () => {
+  return (
+    <View>
+      <Text>This is the settings page</Text>
+    </View>
+  );
+};
+
+export default Setting;
